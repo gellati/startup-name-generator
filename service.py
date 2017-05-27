bd2eeed2ea7aecb268cb42a0463c4e57d33dba57
Omar El-Begawy <omar.elbegawy@gmail.com>
2017-05-27 15:23:47 +0300

    added frontpage

--- service.py
+++ service.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import jsonify
 from NameRnn import *
 import json
@@ -8,16 +8,24 @@ app.debug = True
 nrn = NameRnn()
 
 @app.route("/")
-def checkin():
-    functionality = 'not functional'
-    if nrn is not None:
-        functionality = "functional and healthy"
-    return jsonify(functionality=functionality)
+def mainpage():
+    entries = nrn.get(5)
+    print entries
+    return render_template('show_entries.html', entries=entries)
+
+#def checkin():
+#    functionality = 'not functional'
+#    if nrn is not None:
+#        functionality = "functional and healthy"
+#    return jsonify(functionality=functionality)
 
 @app.route("/get/<int:num>")
 def get(num):
     names = nrn.get(num)
     return jsonify(names=names)
+
+
+
 
 if __name__ == "__main__":
     app.run()
