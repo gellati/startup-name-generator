9ae00e05ca9db3f26c3e125067d5fb5ddcbc73af
Omar El-Begawy <omar.elbegawy@gmail.com>
2017-05-28 02:25:25 +0300

    added loading page and images to results page

--- service.py
+++ service.py
@@ -4,6 +4,8 @@ from NameRnn import *
 import json
 import requests
 
+from PIL import Image, ImageFont, ImageDraw
+
 
 app = Flask(__name__)
 app.debug = True
@@ -16,15 +18,32 @@ def getName():
 #    data = json.loads(response.content)
     return data
 
+# TODO: not working yet
+def writeImageText(image, text, xcoord, ycoord ):
+    font = ImageFont.truetype("/usr/share/fonts/dejavu/DejaVuSans.ttf", 25)
+#    img = Image.new("RGBA", (200,200), (120,20,20))
+    draw = ImageDraw.Draw(img)
+    draw.text((0,0), "This is a test", (255,255,0), font=font)
+    draw = ImageDraw.Draw(image)
+    img.save("a-")
+
+
+
 @app.route("/")
 def mainpage():
     entries = nrn.get(5)
     print entries
     return render_template('landing_page.html')
 
+@app.route("/loading")
+def loading():
+    return render_template('loading_page.html')
+
 @app.route("/results")
 def results():
     name = getName()
+    article_text =  "gets 3 million $ funding"
+    businesscard_text = "Best in what in class"
     return render_template('show_results.html', name=name)
 
 #def checkin():
