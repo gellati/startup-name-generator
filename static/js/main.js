0ec56873b61ed90a3d646b6edb93e9c45c2a44fa
Omar El-Begawy <omar.elbegawy@gmail.com>
2017-05-28 12:54:18 +0300

    randomisation of names in loading page

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,65 @@
       (function makeDiv(){
+        var names = ['Browsi',
+        'CYTIOT',
+        'ApolloShield',
+        'PATH',
+        'Causemo',
+        'Cinchcast',
+        'Zoey',
+        'Women.com',
+        'DevMynd',
+        'GovX',
+        'LeaseAccelerator',
+        'Datami',
+        'Wildfang',
+        'FINCAD',
+        'WebyClip',
+        'doubleTwist',
+        'Jia.com',
+        'PipelineDeals',
+        'Vindi',
+        'Storone',
+        'Olpays',
+        'Bioscale',
+        'Hepstar',
+        'Enflux',
+        'Tribridge',
+        'DevicePilot',
+        'Sweetch',
+        'Roseonly',
+        'Hedgy',
+        'CityBldr',
+        'Flipps',
+        'misterb&b',
+        'Paxos',
+        'Weimob',
+        'ClearScholar',
+        'LumaTax',
+        'Thatgamecompany',
+        'Thunkable',
+        'Transfluent',
+        'Inventorum',
+        'ivee',
+        'PrivacyCheq',
+        'PredicSis',
+        'Pulpix',
+        'SecretSales',
+        'Stratumn',
+        'Bonaverde',
+        'Pushbullet',
+        'Velvetcase',
+        'Giftagram',
+        'Paradromics',
+        'AFAR',
+        'Farmobile',
+        'Travelio',
+        'MedCrypt',
+        'AuditFile',
+        'ClickDimensions'];
+
+        var name =  names[Math.floor(Math.random() * names.length)];
+        console.log(name)
+
           var divsize = 200;
           var color = '#'+ Math.round(0xffffff * Math.random()).toString(16);
           $newdiv = $('<div/>').css({
@@ -6,10 +67,10 @@
               'height':divsize+'px',
               'color': color
           });
-          
+
           var posx = (Math.random() * ($(document).width() - divsize)).toFixed();
           var posy = (Math.random() * ($(document).height() - divsize)).toFixed();
-          $newdiv.append("<h1>JERE<h1>");
+          $newdiv.append("<h1>"+name.toString()+"<h1>");
           $newdiv.css({
               'position':'absolute',
               'left':posx+'px',
@@ -17,6 +78,8 @@
               'display':'none'
           }).appendTo( '#main' ).fadeIn(200).delay(400).fadeOut(400, function(){
             $(this).remove();
-            makeDiv(); 
-          }); 
+            makeDiv();
+          });
+
+
       })();
